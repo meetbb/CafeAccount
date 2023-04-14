f5ccb068cab084d2595fead5ab9c65e50142fcfc
meetbrahmbhatt91 <meetbrahmbhatt91@gmail.com>
2023-04-14 16:17:50 +0530

    Initial Commit

---
+++ CafeAccounts/CafeAccounts.h
@@ -0,0 +1,18 @@
+//
+//  CafeAccounts.h
+//  CafeAccounts
+//
+//  Created by Meet Brahmbhatt on 14/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CafeAccounts.
+FOUNDATION_EXPORT double CafeAccountsVersionNumber;
+
+//! Project version string for CafeAccounts.
+FOUNDATION_EXPORT const unsigned char CafeAccountsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CafeAccounts/PublicHeader.h>
+
+
